ac113390d14269eab711111233df6c0860ed7e93
DOMINIO\gdario <gdario@GERMAN-D>
2017-10-02 15:53:13 -0300

    Agrego ejemplo de conexion a RestApi.

---
+++ Assets/Examples/RestClient/Client/Assets/RESTFactory.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RESTFactory : MonoBehaviour
+{
+
+    private string results;
+
+    public String Results
+    {
+        get
+        {
+            return results;
+        }
+    }
+
+    public WWW GET(string url, System.Action onComplete)
+    {
+
+        WWW www = new WWW(url);
+        StartCoroutine(WaitForRequest(www, onComplete));
+        return www;
+    }
+
+    public WWW POST(string url, Dictionary<string, string> post, System.Action onComplete)
+    {
+        WWWForm form = new WWWForm();
+
+        foreach (KeyValuePair<string, string> post_arg in post)
+        {
+            form.AddField(post_arg.Key, post_arg.Value);
+        }
+
+        WWW www = new WWW(url, form);
+
+        StartCoroutine(WaitForRequest(www, onComplete));
+        return www;
+    }
+
+    private IEnumerator WaitForRequest(WWW www, System.Action onComplete)
+    {
+        yield return www;
+        // check for errors
+        if (www.error == null)
+        {
+            results = www.text;
+            onComplete();
+        }
+        else
+        {
+            Debug.Log(www.error);
+        }
+    }
+}
---
+++ Assets/Examples/RestClient/Client/Assets/RestConect.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RestConect : MonoBehaviour
+{
+    public RESTFactory restFactory;
+    public Button yourButton;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        //restFactory.GET("http://localhost:50306/api/values/", GetResult);
+        restFactory.GET("http://localhost:50306/api/values/5", GetResult);
+    }
+
+    void GetResult()
+    {
+        Debug.Log(restFactory.Results);
+    }
+}
