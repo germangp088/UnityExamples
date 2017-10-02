723cd189e767b2746aa1bcf49af57ff093b2650c
DOMINIO\gdario <gdario@GERMAN-D>
2017-10-02 13:57:08 -0300

    Agrego ejemplo de eventos de mouse.

---
+++ Assets/Examples/InstanciateDestroy/Scripts/Flower.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Flower : MonoBehaviour {
+
+    private SpriteRenderer spriteRenderer;
+    public Sprite blueFlower;
+    public Sprite redFlower;
+
+    // Use this for initialization
+    void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer.sprite == null)
+            spriteRenderer.sprite = blueFlower;
+    }
+
+    /// <summary>
+    /// Cuando se hace un Destroy es recomendado destruir el componente principal gameObject que referencia
+    /// al prefab para liberar la memoria.
+    /// </summary>
+    void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// Evento que se dispara cuando el mouse sale del componente.
+    /// </summary>
+    void OnMouseExit()
+    {
+        spriteRenderer.sprite = blueFlower;
+    }
+
+    /// <summary>
+    /// Evento que se dispara cuando el mouse ingresa al componente.
+    /// </summary>
+    void OnMouseEnter()
+    {
+        spriteRenderer.sprite = redFlower;
+    }
+}
---
+++ Assets/Examples/InstanciateDestroy/Scripts/FlowerFactory.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class FlowerFactory : MonoBehaviour
+{
+    public Flower flower;
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(flower, new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), -1), Quaternion.identity);
+        }
+    }
+}
