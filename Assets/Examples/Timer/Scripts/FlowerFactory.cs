a9baf6776e7fe1ed5f78d1205ee9547603b2bf69
DOMINIO\gdario <gdario@GERMAN-D>
2017-10-02 15:31:21 -0300

    Agrego ejemplos con timers ejes en Z y canvas con imagen y texto.

---
+++ Assets/Examples/Timer/Scripts/FlowerFactory.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+namespace Timer
+{
+    public class FlowerFactory : MonoBehaviour
+    {
+        public Flower flower;
+        // Use this for initialization
+        void Start()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Instantiate(flower, new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), -1), Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/Examples/Timer/Scripts/TimerScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour {
+    private float timer;
+    public Text text;
+    public Text fin;
+    private bool end;
+    // Use this for initialization
+    void Start () {
+        timer = 5;
+        text.text = "5";
+        end = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (end)
+            return;
+        timer -= Time.deltaTime;
+        int round = Mathf.RoundToInt(timer);
+        text.text = round.ToString();
+        if (round <= 0)
+        {
+            foreach (var flower in FindObjectsOfType<Flower>())
+            {
+                flower.GetComponent<CircleCollider2D>().enabled = false;
+                flower.GetComponent<Flower>().enabled = false;
+            }
+            end = true;
+            fin.text = "FIN!!!";
+        }
+    }
+
+    public bool GetEnd()
+    {
+        return end;
+    }
+}
